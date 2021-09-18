a0b97ccea4b66fa1f4668c34403b4a58bea712c6
Sumit <sakhare2690@gmail.com>
2021-09-18 12:47:36 +0530

    code organised

--- and.py
+++ and.py
@@ -4,23 +4,27 @@ from utils.all_utils import prepare_data, save_model
 import pandas as pd
 import numpy as np
 
+def  main(data, eta, epoch, filename):
+
+    df=pd.DataFrame(data)
+    print(df)
+    X,y= prepare_data(df)
+    model=Perceptron(eta=eta, epoch=epoch)
+    model.fit(X,y)
+    _=model.total_loss() # dunmmy vaiable _
+
+    save_model(model, filename=filename)
+
+if __name__ == __name__:
+    AND = {
+        "x1":[0,0,1,1],
+        "x2":[0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA= 0.3
+    EPOCH=10
+
+    main(data=AND, eta=ETA, epoch=EPOCH, filename="and.model")
 
 
-AND = {
-    "x1":[0,0,1,1],
-    "x2":[0,1,0,1],
-    "y": [0,0,0,1],
-}
 
-df=pd.DataFrame(AND)
-
-print(df)
-
-X,y= prepare_data(df)
-ETA= 0.3
-EPOCH=10
-model=Perceptron(eta=ETA, epoch=EPOCH)
-model.fit(X,y)
-_=model.total_loss()
-
-save_model(model, filename="and.model")
